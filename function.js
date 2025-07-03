59496ed66d8d0a72e6c0f073c7d767f5f64ce2b3
Nicholas <gauchodev0ps@gmail.com>
2025-07-03 16:50:15 +0300

    cleaned up nested repo and added files

---
+++ function.js
@@ -0,0 +1,6 @@
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Nicck0");   // Prints: Hello, Nicck0!
+greet("World");    // Prints: Hello, World!
---
+++ practice.js
@@ -0,0 +1,6 @@
+let message="wantam";
+const year=2025;
+var isListening=true;
+console.log(message);   // Prints: Hello, JavaScript!
+console.log(year);      // Prints: 2025
+console.log(isLearning); // Prints: true
