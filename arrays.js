a176d2c5a39d6e4eee826a28221408777b237569
Nicholas <gauchodev0ps@gmail.com>
2025-07-03 17:07:06 +0300

    Arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[0]); // Prints: apple
+console.log(fruits.length); // Prints: 3
+
+// Loop through all items
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
